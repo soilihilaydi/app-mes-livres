4584b1420f029e13f2bb6a821b6286a7c30b4908
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-06 20:32:34 +0200

    ajout du fichier env bien que cela soit trop tard creation de la route pour recuperer les donne de la base de donne avec gestion des erreur ou reponse json si donne recupere

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,37 @@
-// Import des modules express et mysql
+// Import des modules express, mysql et dotenv
 import express from "express"; // Import du module Express
 import mysql from "mysql";     // Import du module MySQL
+import dotenv from "dotenv";   // Import du module dotenv
+
+// Chargement des variables d'environnement à partir du fichier .env
+dotenv.config();
 
 // Création de l'instance Express
 const app = express();
 
-// Configuration de la connexion à la base de données MySQL
+// Configuration de la connexion à la base de données MySQL en utilisant les variables d'environnement
 const db = mysql.createConnection({
-  host: "localhost",      // Adresse du serveur MySQL
-  user: "root",           // Nom d'utilisateur de la base de données
-  password: "jebossela",  // Mot de passe de la base de données
-  database: "mes_livres"  // Nom de la base de données
+  host: process.env.DB_HOST,        // Adresse du serveur MySQL
+  user: process.env.DB_USER,        // Nom d'utilisateur de la base de données
+  password: process.env.DB_PASSWORD,// Mot de passe de la base de données
+  database: process.env.DB_DATABASE // Nom de la base de données
 });
 
 // Définition de la route racine ("/") avec une réponse JSON
 app.get("/", (req, res) => {
   res.json("hello this is the backend"); // Répond avec un message JSON
 });
+
+// Définition de la route "/livres" pour récupérer des données depuis la base de données
+app.get("/livres", (req, res) => {
+  const q = "SELECT * FROM livres; "; // Requête SQL pour sélectionner tous les livres
+  db.query(q, (err, data) => {
+    if (err) return res.json(err); // Gestion des erreurs de la requête SQL
+    return res.json(data);        // Réponse JSON avec les données récupérées
+  });
+});
+
+
 
 // Démarrage du serveur Express sur le port 8800
 app.listen(8800, () => {
