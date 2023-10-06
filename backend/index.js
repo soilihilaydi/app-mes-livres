14511c61c6ac7de68d5d197b37b588105d2f9e49
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-06 19:42:08 +0200

    ajout des commentaires

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,24 @@
-import express  from "express"
-import mysql  from "mysql"
+// Import des modules express et mysql
+import express from "express"; // Import du module Express
+import mysql from "mysql";     // Import du module MySQL
 
+// Création de l'instance Express
+const app = express();
 
-const app = express()
-
+// Configuration de la connexion à la base de données MySQL
 const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"jebossela",
-  database:"mes_livres"
-})
+  host: "localhost",      // Adresse du serveur MySQL
+  user: "root",           // Nom d'utilisateur de la base de données
+  password: "jebossela",  // Mot de passe de la base de données
+  database: "mes_livres"  // Nom de la base de données
+});
 
-app.get("/", (req,res)=>{
-  res.json("hello this is the backend")
-})
+// Définition de la route racine ("/") avec une réponse JSON
+app.get("/", (req, res) => {
+  res.json("hello this is the backend"); // Répond avec un message JSON
+});
 
-
-app.listen(8800, ()=>{
-  console.log("Connected to backend!")
-})
+// Démarrage du serveur Express sur le port 8800
+app.listen(8800, () => {
+  console.log("Connected to backend!"); // Affiche un message lorsque le serveur démarre
+});
