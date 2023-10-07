442ebed04730274c104eac1e3d9710496deed4a5
soilihi laydi <soilihi.laydi@gmail.com>
2023-10-07 19:13:30 +0200

    structuration html et ajout d un lien vers la page add

--- client/src/pages/Livres.jsx
+++ client/src/pages/Livres.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 // Définition du composant fonctionnel "Livres"
 const Livres = () => {
-  
+  // eslint-disable-next-line
   const [livres, setLivres] = useState([])
 
   useEffect(()=>{
     const fetchAllLivres = async ()=>{
       try{
         const res = await axios.get("http://localhost:8800/Livres")
-        console.log(res) 
+        setLivres(res.data)
       }catch(err){
         console.log(err)
       }
@@ -24,10 +25,22 @@ const Livres = () => {
 
 
   return (
-    // Rendu du composant : un simple div contenant le texte "livres"
-    <div>livres</div>
-  )
-}
+  <div>
+      <h1>Mes Livres</h1>
+      <div className="livres" >
+        {livres.map(livre=>(
+          <div className="livre" key={livre.id}>
+              {livre.cover && <img src={livre.cover} alt=""/>}
+              <h2>{livre.title}</h2>
+              <p>{livre.desc}</p>
+              <span>{livre.price}</span>
+          </div>
+        ))}
+      </div>
+      <button><Link to="/add">Ajouter un nouveau livre</Link></button>
+   </div>
+  );
+};
 
 // Export du composant "Livres" en tant que composant par défaut
 export default Livres;
